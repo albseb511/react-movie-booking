0dac881709adfba0e7890f7adf2169daadb7e580
albseb511 <albertsebastian511@gmail.com>
2020-02-04 15:19:20 +0530

    Update: routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import BookingPage from "./BookingPage";
 import BookingSummary from "./BookingSummary";
 import BuyTickets from "./BuyTickets";
@@ -8,7 +8,24 @@ import MovieDetails from "./MovieDetails";
 import OrderSummary from "./OrderSummary";
 
 const Routes = () => {
-  return <>Routes</>;
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact render={Home} />
+        <Route path="/:place/movies/:movieId" render={MovieDetails} />
+        <Route path="/buytickets/:place/:movieId/:date" render={BookingPage} />
+        <Route
+          path="/buytickets/:place/:movieId/:date/seatLayout"
+          render={BuyTickets}
+        />
+        <Route
+          path="/buytickets/:place/:movieId/:date/summary"
+          render={BookingSummary}
+        />
+        <Route path="/payment/:summary" render={OrderSummary} />
+      </Switch>
+    </>
+  );
 };
 
 export default Routes;
